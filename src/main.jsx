de6ed56fbb4496d52be66e0c5293af077dba6912
Ace <adamabdallahayln1@gmail.com>
2026-01-21 11:50:32 +0200

    Adding 404 html

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+const params = new URLSearchParams(window.location.search);
+const redirect = params.get("redirect");
+
+if (redirect) {
+  window.history.replaceState(null, "", "/cedarstech-pricelist" + redirect);
+}
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/cedarstech-pricelist/">
+  <BrowserRouter basename="/cedarstech-pricelist">
     <App />
   </BrowserRouter>
 );
